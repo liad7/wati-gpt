9312c7574363b72602199d6583609b33d1f1353c
ord669 <ord24@gmail.com>
2023-05-22 20:36:01 +0300

    added wati

--- services/api/wati/wati.ts
+++ services/api/wati/wati.ts
@@ -1,45 +1,25 @@
-import axios from "axios"
-
-const BASE_URL = 'https://live-server-105712.wati.io/api/v1/'
-
-const getAllContacts = (): Promise<[]> => {
-    const options = {
-        method: 'GET',
-        url: BASE_URL + 'getContacts',
-        headers: {
-            Authorization: 'Bearer ' + process.env.WATI_AUTH
-        }
-    }
-
-    return axios
-        .request(options)
-        .then(res => {
-            console.log(res.data)
-            return res.data
-        })
-        .catch(console.error)
-}
-
-const getChatHistoryByNumber = (phoneNum: string): Promise<string> => {
-    const options = {
-        method: 'GET',
-        url: `${BASE_URL}getMessages/${phoneNum}`,
-        headers: {
-            Authorization: 'Bearer ' + process.env.WATI_AUTH
-        }
-    }
-
-    return axios
-        .request(options)
-        .then(res => {
-            console.log(res.data)
-            return res.data
-        })
-        .catch(console.error)
-}
-
+import axios from 'axios';
 
 export const watiService = {
-    getAllContacts,
-    getChatHistoryByNumber,
+    getContacts
+}
+
+async function getContacts() {
+    const options = {
+        method: 'GET',
+        url: 'https://live-server-105712.wati.io/api/v1/getContacts',
+        headers: {
+            Authorization: `${process.env.WATI_ACCESS_TOKEN!}`
+        }
+    };
+
+    return axios
+        .request(options)
+        .then(function (response) {
+            return response.data
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+
 }
