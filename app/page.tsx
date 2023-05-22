ab36e6b29cbf326e1ac259cde10f949857a0edee
liad7 <liadgola7@gmail.com>
2023-05-22 19:03:03 +0300

    clean pase.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,8 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <main>
+
     </main>
   )
 }
