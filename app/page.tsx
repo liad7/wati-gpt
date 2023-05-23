6bfdb6597b51da2f7aedbe1dbbcaf4005f9e5e77
ord669 <ord24@gmail.com>
2023-05-23 12:25:44 +0300

    fixed wati

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,16 @@ import { watiService } from '../services/api/wati/wati';
 
 export default async function Home() {
   const watiContact = await watiService.getContacts()
-  console.log('watiContact: ', watiContact.contact_list[0].id);
+  console.log('watiContact: ', watiContact.contact_list[0].wAid);
+
+  const contactPhones = watiContact.contact_list.reduce((acc: any[], curr: { wAid: any; }) => {
+    acc.push(curr.wAid)
+    return acc
+  }, [])
+  console.log('contact_list.length:', watiContact.contact_list.length)
+  console.log('contactPhones: ', contactPhones);
+  console.log('contactPhones.length:', contactPhones.length)
+
   const message: ChatCompletionResponseMessage = await openAiService.getAnswerFromGpt('how are you?')
   const { role, content } = message
   return (
--- services/api/wati/wati.ts
+++ services/api/wati/wati.ts
@@ -23,3 +23,6 @@ async function getContacts() {
         });
 
 }
+
+
+
