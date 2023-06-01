52acab0f385b57f26edd66cf51c864d6fe75dbb1
liad7 <liadgola7@gmail.com>
2023-06-01 17:19:49 +0300

    google translate api service

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 
-import { openAiService } from "../services/api/openAi/openAi";
-import { ChatCompletionResponseMessage } from 'openai/dist/api';
-import { watiService } from '../services/api/wati/wati';
+// import { openAiService } from "../services/api/openAi/openAi";
+// import { ChatCompletionResponseMessage } from 'openai/dist/api';
+// import { watiService } from '../services/api/wati/wati';
 import { Header } from "@/cmps/header/header";
+import { googleTranslateAPIService } from "../services/api/googleTranslateAPI/googleTranslateAPI"
 
 export default async function Home() {
   // const watiContact = await watiService.getContacts()
@@ -15,6 +16,7 @@ export default async function Home() {
 
   // const message: ChatCompletionResponseMessage = await openAiService.getAnswerFromGpt('how are you?')
   // const  {role, content}  = message
+  googleTranslateAPIService.detectLanguage('אחשלי היקר מה איתך נשמה').then(console.log)
 
   return (
     <main className="main-container">
---
+++ services/api/googleTranslateAPI/googleTranslateAPI.ts
@@ -0,0 +1,36 @@
+const axios = require('axios')
+const apiKey = process.env.GOOGLE_TRANSLATE_API_KEY
+export const googleTranslateAPIService = {
+    detectLanguage,
+    translateText,
+}
+
+async function detectLanguage(input: string): Promise<string | undefined> {
+    try {
+        const url = `https://translation.googleapis.com/language/translate/v2/detect?key=${apiKey}&q=${encodeURI(input)}`
+
+        const res = await axios.post(url)
+        const detectedLanguage: string = res.data.data.detections[0][0].language
+        console.log(`Detected language: ${detectedLanguage}`)
+
+        return detectedLanguage
+    } catch (error) {
+        console.error('Error:', error)
+        return undefined
+    }
+}
+
+async function translateText(input: string, sourceLanguage: string): Promise<string | undefined> {
+    try {
+        const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}&q=${encodeURI(input)}&source=${sourceLanguage}&target=en`
+
+        const res = await axios.post(url)
+        const translatedText: string = res.data.data.translations[0].translatedText
+        console.log(`Translated text: ${translatedText}`)
+
+        return translatedText
+    } catch (error) {
+        console.error('Error:', error)
+        return undefined
+    }
+}
