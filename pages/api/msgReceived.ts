b7aa0aaf3d9f56bdfe1d45c47793805f88a68381
liad7 <liadgola7@gmail.com>
2023-06-01 18:29:27 +0300

    handle webhook of message received

--- pages/api/msgReceived.ts
+++ pages/api/msgReceived.ts
@@ -1,12 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import {googleTranslateAPIService } from '../../services/api/googleTranslateAPI/googleTranslateAPI'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         // Process the webhook payload here
         const { body, headers } = req
-
+        const { text } = body
         // Handle the webhook data
         // ...
+        googleTranslateAPIService.detectLanguage(text).then(res => googleTranslateAPIService.translateText(text, res).then(console.log))
+
 
         // Send a response (if required)
         res.status(200).json({ message: 'Webhook received successfully' })
